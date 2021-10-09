dae944bc6a0912a6c2d422d5281410cdaabb6eb1
Walid Wazini <wazini25@gmail.com>
2021-10-10 05:18:58 +0800

    apply Bloc Listener

--- lib/Cubit/counter_cubit.dart
+++ lib/Cubit/counter_cubit.dart
@@ -4,8 +4,8 @@ import 'package:meta/meta.dart';
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0));
+  CounterCubit() : super(CounterState(counterValue: 0, wasIncremented: true));
 
-  void increment() => emit(CounterState(counterValue: state.counterValue + 1));
-  void decrement() => emit(CounterState(counterValue: state.counterValue - 1));
+  void increment() => emit(CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+  void decrement() => emit(CounterState(counterValue: state.counterValue - 1, wasIncremented: false));
 }
